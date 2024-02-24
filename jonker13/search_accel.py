5ab0bfd91ed145cd38bcf40bf57ee127e05ad72d
Dennis Alp <github@dennisalp.com>
2024-02-24 10:39:29 +0100

    finalize tdrpcas

--- jonker13/search_accel.py
+++ jonker13/search_accel.py
@@ -1,5 +1,5 @@
 '''
-2024-02-23, Dennis Alp, dalp@kth.se
+2024-02-24, Dennis Alp, dalp@kth.se
 
 Search for pulsation in XRT 000519 (Jonker et al. 2013).
 
@@ -26,7 +26,6 @@ x['ip'] = 10  # initial prints, how many iterations to print before switching to
 x['pi'] = 500  # printing interval
 x['lim'] = 14  # lowest power to save to top
 x['dt'] = 1.2047  # s, sampling interval
-x['dt'] = 4.8186  # s, sampling interval
 # (tt[-1]-tt[0])/2**14 = 1.2046456746462377 (choose 'dt' slightly higher than this such that a little padding is required)
 x['fm'] = 0.0005  # Hz, minimum frequency, to discard frequencies affected by instrumental effects
 x['ex'] = True  # extend the array to accomodate events boosted beyond the power-of-two array
@@ -34,21 +33,20 @@ x['1ppb'] = False  # At most 1 photon per bin? (i.e. bins >> photons)
 
 # Accelerated
 x['mode'] = 'accel'
-x['mc'] = 100000000  # Msun, companion mass
-x['fr'] = .99  # maximum fraction of orbit of the segment
-x['tm'] = 9e9  # s, shortest segment
+x['mc'] = 100  # Msun, companion mass
+x['fr'] = .05  # maximum fraction of orbit of the segment
+x['tm'] = 1e4  # s, shortest segment
 x['nm'] = 300  # fewest counts in a segment
 x['as'] = 1.0  # acceleration step size, units of frequency bins
 
 # Binary scan
-# x['mode'] = 'scan'
-# x['ctr'] = 0
-# x['nMp'] = 1  # number of pulsar masses
-# x['nMc'] = 30  # number of companion masses
-# x['nPb'] = 1  # number of binary periods
-# x['nTT'] = 20  # number of epoch of periastron passage
-# x['nEc'] = 20  # number of eccentricity steps (1 for circular)
-# x['nWW'] = 20  # number of longitude of periastron
+x['mode'] = 'scan'
+x['nMp'] = 3  # number of pulsar masses
+x['nMc'] = 3  # number of companion masses
+x['nPb'] = 3  # number of binary periods
+x['nTT'] = 3  # number of epoch of periastron passage
+x['nEc'] = 3  # number of eccentricity steps (1 for circular)
+x['nWW'] = 3  # number of longitude of periastron
 
 # Source files
 x['a1'] = 'tt_prepared.csv'
@@ -56,6 +54,7 @@ x['ext'] = '.'+x['a1'].split('.')[-1]
 files = [x['a1']]
 
 # Preparation
+x['ctr'] = 0
 x['p0'] = 2*x['dt']
 x['n0'] = 1/x['p0']
 x['buf'] = 1000000
